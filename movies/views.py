f43bf07b65d7e4463382011aa0649933b407da08
Naim Miah <naim_ruet@yahoo.com>
2019-02-24 18:03:40 +0600

    Push to git

---
+++ movies/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+
+# Create your views here.
+
+
+def index(request):
+    movies = Movie.objects.all()
+    # Movie.objects.filter(release_year=1992)
+    # Movie.objects.get(id=1)
+    # output = ', '.join([m.title for m in movies])
+    return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
+
+    # try:
+    #     movie = Movie.objects.get(pk=movie_id)
+    #     return render(request, 'movies/detail.html', {'movie': movie})
+    # except Movie.DoesNotExist:
+    #     raise Http404()
